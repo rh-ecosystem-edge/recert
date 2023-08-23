a16719eafe5ae0ecad4ffd8c1300cebdd37dbf95
Omer Tuchfeld <omer@tuchfeld.dev>
2023-08-23 09:20:24 +0200

    Move comment

--- src/k8s_etcd.rs
+++ src/k8s_etcd.rs
@@ -15,18 +15,18 @@ pub(crate) struct EtcdResult {
     pub(crate) value: Vec<u8>,
 }
 
+/// An etcd client wrapper backed by an in-memory hashmap. All reads are served from memory, with
+/// fallback to actual etcd. All writes are strictly to memory. Also supports eventually committing
+/// to an actual etcd instance of kubernetes, transparently encoding and decoding YAMLs with ouger.
+/// Used by recert as a cache to dramatically speed up the process of certificate and key
+/// regeneration, as we we don't have to go through ouger and etcd for every single certificate and
+/// key access.
 pub(crate) struct InMemoryK8sEtcd {
     etcd_client: Arc<EtcdClient>,
     etcd_keyvalue_hashmap: Mutex<HashMap<String, Vec<u8>>>,
     deleted_keys: Mutex<HashSet<String>>,
 }
 
-// An etcd client wrapper backed by an in-memory hashmap. All reads are served from memory, with
-// fallback to actual etcd. All writes are strictly to memory. Also supports eventually committing
-// to an actual etcd instance of kubernetes, transparently encoding and decoding YAMLs with ouger.
-// Used by recert as a cache to dramatically speed up the process of certificate and key
-// regeneration, as we we don't have to go through ouger and etcd for every single certificate and
-// key access.
 impl InMemoryK8sEtcd {
     pub(crate) fn new(etcd_client: EtcdClient) -> Self {
         Self {
