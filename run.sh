a62f43e3fb21311a356f330f83cc30c4581d99bc
Omer Tuchfeld <omer@tuchfeld.dev>
2023-08-25 14:59:09 +0200

    Fix run.sh

--- run.sh
+++ run.sh
@@ -2,34 +2,24 @@
 
 set -uo pipefail
 
-SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
-
 REPO_DIR=/home/$USER/repos/recert
-QCOW2_DIR=/home/$USER/Documents/model6
-RELEASE_IMAGE=quay.io/openshift-release-dev/ocp-release:4.13.0-x86_64
-#####################################
-
 CLUSTER_DIR="$REPO_DIR"/cluster-files
 BACKUP_CLUSTER_DIR="$REPO_DIR"/cluster-files-backup
-ETCD_RESOURCES="secrets configmaps validatingwebhookconfigurations apiregistration.k8s.io/apiservices"
-ETCD_CRDS="machineconfiguration.openshift.io/machineconfigs"
+
+function dump {
+	dumpdir="$1"
+    echo "Dumping etcd to $dumpdir"
+	mkdir -p "$dumpdir"
+	rm -rf "$dumpdir"
+	endpoints="--endpoints=127.0.0.1:2379"
+	for key in $(etcdctl $endpoints get --prefix / --keys-only); do
+		mkdir -p $(dirname "$dumpdir/$key")
+		(etcdctl $endpoints get --print-value-only "$key" | head -c -1 | ouger decode >"$dumpdir/$key.yaml") &
+	done
+}
 
 # First dump original etcd
-dumpdir="$BACKUP_CLUSTER_DIR"/etcd_dump
-mkdir -p $dumpdir
-rm -rf $dumpdir
-endpoints="--endpoints=127.0.0.1:2379"
-for key in $(etcdctl $endpoints get /--prefix --keys-only); do
-    mkdir -p $(dirname "$dumpdir"/$key)
-    (etcdctl $endpoints get --print-value-only $key | head -c -1 | ouger decode >"$dumpdir"/$key.yaml) &
-done
-
-for kind in $(echo $ETCD_CRDS); do
-	for key in $(etcdctl $endpoints get /kubernetes.io/"$kind"/ --prefix --keys-only); do
-		mkdir -p $(dirname $dumpdir/$key)
-		(etcdctl $endpoints get --print-value-only $key | head -c -1 | toyaml >"$dumpdir"/$key.yaml) &
-	done
-done
+dump "$BACKUP_CLUSTER_DIR/etcd_dump"
 
 wait
 
@@ -46,21 +36,7 @@ cargo run --manifest-path "$REPO_DIR"/Cargo.toml --release -- \
 	--cluster-rename new-name,foo.com
 
 # Dump etcd after changes
-dumpdir="$CLUSTER_DIR"/etcd_dump
-mkdir -p "$dumpdir"
-rm -rf "$dumpdir"
-endpoints="--endpoints=127.0.0.1:2379"
-for kind in $(echo $ETCD_RESOURCES); do
-	for key in $(etcdctl $endpoints get /kubernetes.io/"$kind"/ --prefix --keys-only); do
-		mkdir -p $(dirname "$dumpdir"/$key)
-		(etcdctl $endpoints get --print-value-only $key | head -c -1 | ouger decode >"$dumpdir"/$key.yaml) &
-	done
-done
-for kind in $(echo $ETCD_CRDS); do
-	for key in $(etcdctl $endpoints get /kubernetes.io/"$kind"/ --prefix --keys-only); do
-		mkdir -p $(dirname $dumpdir/$key)
-		(etcdctl $endpoints get --print-value-only $key | head -c -1 | toyaml >"$dumpdir"/$key.yaml) &
-	done
-done
+dump "$CLUSTER_DIR/etcd_dump"
 
 meld "$BACKUP_CLUSTER_DIR" "$CLUSTER_DIR"
+
