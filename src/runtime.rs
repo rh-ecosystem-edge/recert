1203ba991698a605c5b7eaeed896dcf3ae8d98f1
Omer Tuchfeld <omer@tuchfeld.dev>
2023-09-07 11:16:49 +0200

    More verbose getrlimit setrlimit with error checking

--- src/runtime.rs
+++ src/runtime.rs
@@ -1,25 +1,42 @@
-use anyhow::{Context, Result};
+use anyhow::{bail, Context, Result};
 
-pub(crate) fn set_max_open_files_limit() {
+pub(crate) fn set_max_open_files_limit() -> Result<()> {
     let mut current_limit = libc::rlimit { rlim_cur: 0, rlim_max: 0 };
-    unsafe {
-        libc::getrlimit(libc::RLIMIT_NOFILE, &mut current_limit);
+    match unsafe { libc::getrlimit(libc::RLIMIT_NOFILE, &mut current_limit) } {
+        0 => {}
+        _ => {
+            bail!("Failed to get current max open files limit");
+        }
     }
+
+    println!(
+        "Current max open files soft {} hard {}",
+        current_limit.rlim_cur, current_limit.rlim_max
+    );
 
     let new_limit = libc::rlimit {
         rlim_cur: current_limit.rlim_max,
         rlim_max: current_limit.rlim_max,
     };
 
-    unsafe {
-        libc::setrlimit(libc::RLIMIT_NOFILE, &new_limit);
+    println!("Setting max open files {} hard {}", new_limit.rlim_cur, new_limit.rlim_max);
+
+    match unsafe {
+        libc::setrlimit(libc::RLIMIT_NOFILE, &new_limit)
+    } {
+        0 => {}
+        _ => {
+            bail!("Failed to set max open files limit");
+        }
     }
+
+    Ok(())
 }
 
 pub(crate) fn prepare_tokio_runtime(threads: Option<usize>) -> Result<tokio::runtime::Runtime> {
     // Using tokio we we usually need to open a lot of files. Set the max open files limit to the
     // maximum allowed by the kernel
-    set_max_open_files_limit();
+    set_max_open_files_limit().context("Setting open file limits to max")?;
 
     Ok(if let Some(threads) = threads {
         tokio::runtime::Builder::new_multi_thread()
