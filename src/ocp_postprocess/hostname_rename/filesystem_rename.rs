b412053bb799a119599e28871ba23c2f6543a927
Michail Resvanis <mresvani@redhat.com>
2024-01-26 11:09:24 +0100

    Replace hostname in kube-apiserver-startup-monitor-pod
    Signed-off-by: Michail Resvanis <mresvani@redhat.com>

--- src/ocp_postprocess/hostname_rename/filesystem_rename.rs
+++ src/ocp_postprocess/hostname_rename/filesystem_rename.rs
@@ -109,3 +109,69 @@ pub(crate) async fn fix_filesystem_etcd_scripts_cluster_backup_sh(original_hostn
 
     Ok(())
 }
+
+pub(crate) async fn fix_filesystem_kapi_startup_monitor_pod(hostname: &str, dir: &Path) -> Result<()> {
+    join_all(file_utils::globvec(dir, "**/kube-apiserver-startup-monitor-pod.yaml")?.into_iter().map(|file_path| {
+        let kapi_startup_monitor_pod_path = file_path.clone();
+        let hostname = hostname.to_string();
+        tokio::spawn(async move {
+            async move {
+                let contents = read_file_to_string(&file_path)
+                    .await
+                    .context("reading kube-apiserver-startup-monitor-pod.yaml")?;
+
+                let mut pod: Value = serde_json::from_str(&contents).context("parsing kube-apiserver-startup-monitor-pod.yaml")?;
+
+                rename_utils::fix_kapi_startup_monitor_pod_container_args(&mut pod, &hostname).context("fixing kube-apiserver-startup-monitor-pod.yaml")?;
+
+                commit_file(file_path, serde_json::to_string(&pod).context("serializing kube-apiserver-startup-monitor-pod.yaml")?)
+                    .await
+                    .context("writing kube-apiserver-startup-monitor-pod.yaml to disk")?;
+
+                anyhow::Ok(())
+            }
+            .await
+            .context(format!("fixing kube-apiserver-startup-monitor-pod.yaml {:?}", kapi_startup_monitor_pod_path))
+        })
+    }))
+    .await
+    .into_iter()
+    .collect::<Result<Vec<_>, _>>()?
+    .into_iter()
+    .collect::<Result<Vec<_>>>()?;
+
+    Ok(())
+}
+
+pub(crate) async fn fix_filesystem_kapi_startup_monitor_configmap_pod_yaml(original_hostname: &str, hostname: &str, dir: &Path) -> Result<()> {
+    join_all(file_utils::globvec(dir, "**/kube-apiserver-pod*/configmaps/kube-apiserver-pod/kube-apiserver-startup-monitor-pod.yaml")?.into_iter().map(|file_path| {
+        let kapi_startup_monitor_pod_path = file_path.clone();
+        let original_hostname = original_hostname.to_string();
+        let hostname = hostname.to_string();
+        tokio::spawn(async move {
+            async move {
+                let contents = read_file_to_string(&file_path)
+                    .await
+                    .context("reading kube-apiserver-startup-monitor-pod.yaml")?;
+
+                commit_file(
+                    file_path,
+                    rename_utils::fix_kapi_startup_monitor_pod_yaml(&contents, &original_hostname, &hostname).context("fixing kube-apiserver-startup-monitor-pod.yaml")?,
+                )
+                .await
+                .context("writing kube-apiserver-startup-monitor-pod.yaml to disk")?;
+
+                anyhow::Ok(())
+            }
+            .await
+            .context(format!("fixing kube-apiserver-startup-monitor-pod.yaml {:?}", kapi_startup_monitor_pod_path))
+        })
+    }))
+    .await
+    .into_iter()
+    .collect::<Result<Vec<_>, _>>()?
+    .into_iter()
+    .collect::<Result<Vec<_>>>()?;
+
+    Ok(())
+}
